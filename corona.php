510bd10f815aedbe5dc2b226d435b7c5291d7db6
Lucas Gomes <lulinux2109@gmail.com>
2020-05-06 00:46:12 -0300

    Tela de login com carregamento

---
+++ corona.php
@@ -0,0 +1,39 @@
+<?php
+	for($i = 0; $i <= 300000000; $i++)
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login - SUS</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<style type="text/css">
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		body {
+			background-color: #FAFAFA;
+			color: #FFF;
+			font-weight: bold;
+			font-family: 'Trebuchet MS', sans-serif;
+		}
+		
+	</style>
+</head>
+<body>
+	<!-- Iniciando o Código -->
+	<div class="container">
+		
+	</div>
+	<!-- Fim Html -->
+	<!-- Javascript -->
+	<script type="text/javascript">
+
+		
+			
+	</script>
+</body>
+</html>
